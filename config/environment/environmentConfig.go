dbf8a0642467947311e625895bccea14a54d1c50
nurefendi <nur.efendi2505@gmail.com>
2025-04-26 22:32:00 +0700

    update load env

--- config/environment/environmentConfig.go
+++ config/environment/environmentConfig.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"strings"
 	"auth-service/common/constants"
-	"errors"
 
 	"github.com/joho/godotenv"
 	"github.com/gofiber/fiber/v2/log"
@@ -13,19 +12,12 @@ import (
 
 func loadEnvVariables(key string) error {
 	var err error
-	switch key {
-	case constants.LOCAL:
+	if constants.LOCAL == key {
 		err = godotenv.Load(".local.env")
-	case constants.PRODUCTION:
-		err = godotenv.Load(".production.env")
-	default:
-		return errors.New("unable to find configuration for env=" + key)
 	}
-
 	if err != nil {
 		return err
 	}
-
 	log.Info(fmt.Sprintf("Application is running on %s environment", key))
 	return nil
 }
